7d7ac80a7edda0e2d61428a4e170449fef00baa2
Denis Borisov <mai.borisov.denis@gmail.com>
2022-12-17 22:35:27 +0300

    first commit

---
+++ src/main/java/com/example/fantasticstreamcharm/FantasticStreamCharmApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasticstreamcharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticStreamCharmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasticStreamCharmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fantasticstreamcharm/FantasticStreamCharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasticstreamcharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticStreamCharmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
